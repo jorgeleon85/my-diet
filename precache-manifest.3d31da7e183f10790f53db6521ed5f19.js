5733dc2d95d7cab3864f92617f0a4b5227f30780
Jorge León <jorgeleon85@gmail.com>
2020-02-25 21:59:17 -0600

    Updates

---
+++ precache-manifest.3d31da7e183f10790f53db6521ed5f19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e17387204779cf2880e3ce972ceef3",
+    "url": "/my-diet/index.html"
+  },
+  {
+    "revision": "9dc690ac3968e42aba05",
+    "url": "/my-diet/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f37fe4214c1c7a57ad7d",
+    "url": "/my-diet/static/js/2.5d8d2006.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-diet/static/js/2.5d8d2006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc690ac3968e42aba05",
+    "url": "/my-diet/static/js/main.087d3352.chunk.js"
+  },
+  {
+    "revision": "383eb9b395d2abfc598d",
+    "url": "/my-diet/static/js/runtime-main.79910df8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-diet/static/media/logo.5d5d9eef.svg"
+  }
+]);
